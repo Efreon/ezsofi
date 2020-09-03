718020ada714f499d3a430acfd52fb50f8e098b2
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-03 17:28:35 +0200

    W1D3 exercise solutions

---
+++ week-01/day-3/Exercises_W1D3/1_HelloMe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Zsófi!");
+        }
+    }
+}
