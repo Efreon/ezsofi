f40b127ae5a6cd32b2ef89b601ccd05415a7302d
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-04 18:15:22 +0200

    W1D4 exercises

---
+++ week-01/day-4/Exercises/06_PartyIndicator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _06_PartyIndicator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that asks for two numbers
+            // The first number represents the number of girls that comes to a party, the
+            Console.Write("Number of girls: ");
+            int numGirls = Convert.ToInt32(Console.ReadLine());
+
+            // second the boys
+            Console.Write("Number of boys: ");
+            int numBoys = Convert.ToInt32(Console.ReadLine());
+
+            int totalParticipants = numGirls + numBoys;
+
+            // It should print: Sausage party
+            // If no girls are coming, regardless the count of the people
+            if (numGirls == 0)
+            {
+                Console.Write("Sausage party");
+            }
+
+            // It should print: Average party...
+            // If there are less people coming than 20
+            else if (totalParticipants < 20)
+            {
+                Console.WriteLine("Average party...");
+            }
+
+            // It should print: Quite cool party!
+            // It there are more than 20 people coming but the girl - boy ratio is not 1-1
+            else if (totalParticipants > 20 && numGirls != numBoys)
+            {
+                Console.WriteLine("Quite cool party!");
+            }
+
+            // It should print: The party is exellent!
+            // If the the number of girls and boys are equal and there are more people coming than 20
+            else if (totalParticipants > 20 && numGirls == numBoys)
+            {
+                Console.WriteLine("The party is excellent!");
+            }
+        }
+    }
+}
---
+++ week-01/day-4/Exercises/09_IWontCheatOnTheExams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09_IWontCheatOnTheExams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+           for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("I won't cheat on the exam!");
+            }
+        }
+    }
+}
