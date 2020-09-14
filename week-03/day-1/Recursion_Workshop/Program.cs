cbd5b41dd6cc30f9d99f8dce9cf78e2a605c42fc
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-14 11:02:46 +0200

    Number adder exercise solution

---
+++ week-03/day-1/Recursion_Workshop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Recursion_Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberAdder(4));
+        }
+
+        public static long NumberAdder(int n)
+        {
+            if (n==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n + NumberAdder(n - 1);
+            }
+        }
+    }
+}
