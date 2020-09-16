d59653663d1e5937e6137628926b87f1302dc8fc
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-16 15:07:07 +0200

    Sharpie exercise solution

---
+++ week-03/day-3/ObjectOrientedProgramming/Sharpie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgramming
+{
+    class Sharpie
+    {
+        public string color;
+        public float width;
+        public float inkAmount = 100;
+
+        public Sharpie(string color, float width)
+        {
+            this.color = color;
+            this.width = width;
+        }
+
+        public void Use()
+        {
+            inkAmount--;
+        }
+    }
+}
