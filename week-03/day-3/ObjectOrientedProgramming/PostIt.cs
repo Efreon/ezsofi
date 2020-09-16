8d4f04f8867d88c8a558536f682cf3194404d6d0
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-16 15:05:41 +0200

    PostIt exercise solution

---
+++ week-03/day-3/ObjectOrientedProgramming/PostIt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgramming
+{
+    public class PostIt
+    {
+        string backgroundColor;
+        string text;
+        string textColor;
+
+        public PostIt(string backgroundColor, string text, string textColor)
+        {
+            this.backgroundColor  = backgroundColor;
+            this.text = text;
+            this.textColor = textColor;
+        }
+    }
+}
