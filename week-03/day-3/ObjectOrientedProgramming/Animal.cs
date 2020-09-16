0fb01d42d073571950a3cedd01f235a14227b9aa
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-16 15:06:44 +0200

    Animal exercise solution

---
+++ week-03/day-3/ObjectOrientedProgramming/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgramming
+{
+    public class Animal
+    {
+        public int hunger = 50;
+        public int thirst = 50;
+
+        public Animal(int hunger, int thirst)
+        {
+            this.hunger = hunger;
+            this.thirst = thirst;
+        }
+
+        public void Eat()
+        {
+            hunger++;
+        }
+        public void Drink()
+        {
+            thirst--;
+        }
+
+        public void Play()
+        {
+            thirst++;
+            hunger--;
+        }
+    }
+}
