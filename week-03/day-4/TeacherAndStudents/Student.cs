431c4537e3225e1f0e25f13d4d3394a3b921cf34
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-17 12:54:17 +0200

    Teacher and Students exercise solution

---
+++ week-03/day-4/TeacherAndStudents/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeacherAndStudents
+{
+    public class Student
+    {
+        public Student()
+        {
+        }
+        public void learn()
+        {
+            Console.WriteLine("the student is learning something new");
+        }
+
+        public void question(Teacher teacher)
+        {
+            teacher.answer();
+        }
+    }
+}
---
+++ week-03/day-4/TeacherAndStudents/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeacherAndStudents
+{
+    public class Teacher
+    {
+        
+        public Teacher()
+        {
+
+        }
+        public void teach(Student student)
+        {
+            student.learn();
+        }
+
+        public void answer ()
+        {
+            Console.WriteLine("the teacher is answering a question");
+        }
+    }
+}
