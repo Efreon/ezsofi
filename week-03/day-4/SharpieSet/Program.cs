bdf63171862db99bac8c78c463406a21cd49a2d4
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-17 14:44:36 +0200

    Sharpie set exercise solution

---
+++ week-03/day-4/SharpieSet/Program.cs
@@ -0,0 +1,31 @@
+﻿using ObjectOrientedProgramming;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace SharpieSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sharpieTest = new SharpieSet();
+            sharpieTest.Add(new Sharpie("yellow", 100));
+            sharpieTest.Add(new Sharpie("blue", 100));
+            sharpieTest.Add(new Sharpie("black", 100));
+            sharpieTest.Add(new Sharpie("green", 100));
+
+            foreach (var sharpie in sharpieTest.Sharpies)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    sharpie.Use();
+                }
+            }
+
+            Console.WriteLine(sharpieTest.CountUsable());
+            sharpieTest.RemoveTrash();
+            Console.WriteLine(sharpieTest.CountUsable());
+        }
+    }
+}
---
+++ week-03/day-4/SharpieSet/SharpieSet.cs
@@ -0,0 +1,52 @@
+﻿using ObjectOrientedProgramming;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SharpieSet
+{
+    // Create SharpieSet class
+        // it contains a list of Sharpie
+        // Add method CountUsable() -> sharpie is usable if it has ink in it
+        // Add method RemoveTrash() -> removes all unusable sharpies
+    class SharpieSet
+    {
+        public List<Sharpie> Sharpies;
+
+        public SharpieSet()
+        {
+           Sharpies = new List<Sharpie>();
+        }
+
+        public void Add(Sharpie sharpie)
+        {
+            Sharpies.Add(sharpie);
+        }
+
+        public int CountUsable()
+        {
+            int countSharpie = 0;
+            for (int i=0; i < Sharpies.Count; i++)
+            {
+                if (Sharpies[i].inkAmount > 0)
+                {
+                    countSharpie++;
+                }
+            }
+            return countSharpie;
+        }
+
+        public void RemoveTrash()
+        {
+            for (int i=0; i < Sharpies.Count; i++)
+            {
+                if (Sharpies[i].inkAmount ==0)
+                {
+                    Sharpies.RemoveAt(i);
+                }
+            }
+        }
+
+    }
+}
