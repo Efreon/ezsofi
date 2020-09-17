fcb712e63d2525b86eb7fbe4080d68a1cd13c281
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-17 09:59:04 +0200

    Fleet of Things exercise solution

---
+++ week-03/day-4/FleetOfThings/Fleet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FleetOfThings
+{
+    class Fleet
+    {
+        private List<Thing> Things;
+
+        public Fleet()
+        {
+            Things = new List<Thing>();
+        }
+
+        public void Add(Thing thing)
+        {
+            Things.Add(thing);
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Things.Count; i++)
+            {
+                if (Things[i].getCompleted())
+                {
+                    Console.WriteLine($"{i + 1}. [x] {Things[i].getName()}");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{i + 1}. [ ] {Things[i].getName()}");
+                }
+            }
+
+        }
+    }
+}
---
+++ week-03/day-4/FleetOfThings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FleetOfThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - You have the `Thing` class
+            // - You have the `Fleet` class
+            // - You have the `FleetOfThings` class with a `Main` method
+            // - Download those, use those
+            // - In the Main method create a fleet
+            var fleet = new Fleet();
+
+            // - Achieve this output:s
+            // Create a fleet of things to have this output:
+            // 1. [ ] Get milk
+            // 2. [ ] Remove the obstacles
+            // 3. [x] Stand up
+            // 4. [x] Eat lunch
+            // Hint: You have to create a Print method also
+
+
+            var getMilk = new Thing("Get milk");
+            var obstacles = new Thing("Remove the obstacles");
+            var standUp = new Thing("Stand up");
+            var eatLunch = new Thing("Eat lunch");
+            eatLunch.Complete();
+            standUp.Complete();
+
+            fleet.Add(getMilk);
+            fleet.Add(obstacles);
+            fleet.Add(standUp);
+            fleet.Add(eatLunch);
+
+            fleet.Print();
+        }
+    }
+}
