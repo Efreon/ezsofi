327aa2c8c4e35cc3cec4c2e5a4e1a1e2759478b7
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-18 16:57:53 +0200

    Count Letters exercise

---
+++ week-03/day-5/NewCalculatorTest/CountLettersTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LetterCounting
+{
+    public class CountLettersTest
+    {
+        [Fact]
+        public void ContLettersTesting()
+        {
+            // arrange
+            var inputText = new CountLetters("zsófi");
+
+            // act
+            var testText = inputText.getLettersCounted();
+            var expected = new Dictionary<char, int>() {    { 'z', 1},
+                                                            {'s', 1 },
+                                                            { 'ó', 1},
+                                                            { 'f', 1},
+                                                            {'i', 1 }};
+            //assert
+            Assert.Equal(expected, testText);
+        }
+
+        [Fact]
+        public void ContLettersMultipleTesting()
+        {
+            // arrange
+            var inputText = new CountLetters("baasadsaasknrjraenra");
+
+            // act
+            var testText = inputText.getLettersCounted();
+            var expected = new Dictionary<char, int>() {    {'b', 1},
+                                                            {'a', 7 },
+                                                            {'s', 3},
+                                                            {'d', 1},
+                                                            {'k', 1 },
+                                                            {'n', 2 },
+                                                            {'r', 3 },
+                                                            {'j', 1 },
+                                                            {'e', 1 }};
+            //assert
+            Assert.Equal(expected, testText);
+        }
+
+
+    }
+}
---
+++ week-03/day-5/Testing/CountLetters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetterCounting
+{
+    public class CountLetters
+    {
+        // Write a function, that takes a string as an argument
+        // and returns a dictionary with all letters in the string as keys,
+        // and numbers as values that shows how many occurrences there are.
+
+        public string Text; //  { get; private set; }
+
+        public CountLetters(string text)
+        {
+            Text = text;
+        }
+        public Dictionary<char, int> getLettersCounted()
+        {
+            Dictionary<char, int> countedLetters = new Dictionary<char, int>();
+
+            for (int i=0; i < Text.Length; i++)
+            {
+                if (countedLetters.ContainsKey(Text[i]))
+                {
+                    countedLetters[Text[i]]++;
+                }
+                else
+                {
+                    countedLetters.Add(Text[i], 1);
+                }
+            }
+            return countedLetters;
+        }
+    }
+}
