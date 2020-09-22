0e84100c99b461c3a0d41568f6219f1c875c5a60
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-22 17:11:24 +0200

    Printable Interface exercise

---
+++ week-04/day-2/PrintableInterface/Domino.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintableInterface
+{
+    class Domino : IPrintable
+    {
+        private int sideA;
+        private int sideB;
+
+        public int SideA { get; protected set; }
+        public int SideB { get; protected set; }
+
+        public void PrintAllFields()
+        {
+            Console.WriteLine($"Domino A side: {sideA}, B side: {SideB}");
+        }
+    }
+}
---
+++ week-04/day-2/PrintableInterface/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintableInterface
+{
+    class Todo : IPrintable
+    {
+        private string taskName;
+        private string priority;
+        private bool isDone;
+        
+        protected string TaskName { get; set; }
+        protected string Priority { get; set; }
+        protected bool IsDone { get; set; }
+
+        public void PrintAllFields()
+        {
+            Console.WriteLine($"Task: {TaskName} | Priority: {Priority} | Done: {IsDone}");
+        }
+    }
+}
