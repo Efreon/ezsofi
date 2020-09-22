6c7463e09b9ea08fbc8262d9b2c8ba5069ac772f
ezsofi <e.zsofiakovacs@gmail.com>
2020-09-22 13:12:47 +0200

    Cloning John to JohnTheClone

---
+++ week-04/day-2/GreenFoxOrganization/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreenFoxOrganization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newStudent = new Student();
+            var newStudentCloned = newStudent.Clone() as Student;
+
+            Console.WriteLine(newStudent.Age);
+            Console.WriteLine(newStudentCloned.Age);
+
+            var John = new Student("John", 20, "male", "BME");
+            var JohnTheClone = John.Clone() as Student;
+            Console.WriteLine($"{John.Name}, a {John.Age} yers old {John.Gender} from {John.PreviousOrganization}");
+            Console.WriteLine($"{JohnTheClone.Name}, a {JohnTheClone.Age} yers old {JohnTheClone.Gender} from {JohnTheClone.PreviousOrganization}");
+        }
+    }
+}
---
+++ week-04/day-2/GreenFoxOrganization/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Cache;
+using System.Text;
+
+namespace GreenFoxOrganization
+{
+    class Student : Person, ICloneable
+    {
+        // FIELDS
+        private string previousOrganization;
+        private int skippedDays;
+
+        public string PreviousOrganization { get; protected set; }
+        public int SkippedDays { get; protected set; }
+
+        // CONSTRUCTORS
+        public Student(string name, int age, string gender, string previousOrganization) :base(name, age, gender)
+        {
+            SkippedDays = 0;
+        }
+        public Student() :base()
+        {
+            PreviousOrganization = "The School of Life";
+            SkippedDays = 0;
+        }
+
+        // METHODS
+        public override void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, a {Age} year old {Gender} from {PreviousOrganization} who skipped {SkippedDays} days from the course already.");
+        }
+        public override void GetGoal()
+        {
+            Console.WriteLine("Be a junior software developer.");
+        }
+        public void SkipDays(int numberOfDays)
+        {
+            SkippedDays += numberOfDays;
+        }
+        public object Clone()
+
+        {
+            return new Student( Name,  Age,  Gender,  PreviousOrganization);
+        }
+
+    }
+}
