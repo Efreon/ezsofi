068ed113b21c76117cbbff7f39f4e71841fdce24
ezsofi <e.zsofiakovacs@gmail.com>
2020-10-14 14:05:30 +0200

    Implement Hello World REST App

---
+++ week-06/day-3/MyApplication/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApplication.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from the Controller!";
+        }
+    }
+}
---
+++ week-06/day-3/MyApplication/Controllers/RestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyApplication.Models;
+
+namespace MyApplication.Controllers
+{
+    [Route("api")]
+    public class RestController : Controller
+    {
+        [HttpGet("greeting")]
+        public Greeting Greet()
+        {
+            var greetObj = new Greeting(1, "Hello, World!");
+            return greetObj;
+        }
+    }
+}
