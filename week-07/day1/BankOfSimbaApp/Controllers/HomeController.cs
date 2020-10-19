bbfbb9697e4368c51c5d420a05cdb344ca310787
ezsofi <e.zsofiakovacs@gmail.com>
2020-10-19 15:08:21 +0200

    Shows one account and formats balance

---
+++ week-07/day1/BankOfSimbaApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankOfSimbaApp.Models;
+using BankOfSimbaApp.Models.ViewModels;
+using BankOfSimbaApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankOfSimbaApp.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public AccountServices service {get; set;}
+
+        public HomeController()
+        {
+            service = new AccountServices();
+        }
+        [HttpGet("show")]
+        public IActionResult OneAccount()
+        {
+            var account = new BankAccount("Simba", 2000, "lion");
+            var model = new OneAccountViewModel(account);
+
+            return View(model);
+        }
+        /*
+        [HttpGet("show")]
+        public IActionResult Index()
+        {
+            // var account = new BankAccount("Simba", 2000, "lion");
+            // return View(account);
+            var accounts = service.ReadData();
+            var model = new AccountViewModel(accounts);
+
+            return View(model);
+        }
+         */
+    }
+}
---
+++ week-07/day1/BankOfSimbaApp/Models/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimbaApp.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string AnimalType { get; set; }
+        public string BalanceTwoDecimals { get; set; }
+
+        public BankAccount(string name, double balance, string animalType)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+            BalanceTwoDecimals = String.Format("{0:0.00}", balance);
+
+        }
+
+    }
+}
