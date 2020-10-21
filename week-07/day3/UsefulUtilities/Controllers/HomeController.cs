9d310ba4fa3adea8b549cab57d035acddcfda567
ezsofi <e.zsofiakovacs@gmail.com>
2020-10-21 18:02:15 +0200

    Homecontroller for Useful Utilities

---
+++ week-07/day3/UsefulUtilities/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UsefulUtilities.Services;
+
+namespace UsefulUtilities.Controllers
+{
+    public class HomeController : Controller
+    {
+        public UtilityService service { get; set; }
+        public HomeController()
+        {
+            service = new UtilityService();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ week-07/day3/UsefulUtilities/Services/UtilityService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsefulUtilities.Services
+{
+    public class UtilityService
+    {
+        private readonly List<string> colors;
+        private readonly Random random;
+
+        public UtilityService()
+        {
+            colors = new List<string>
+        {
+            "red",
+            "blue",
+            "lime",
+            "orange",
+            "magenta"
+        };
+
+            random = new Random();
+        }
+
+        public string RandomColor()
+        {
+            return colors[random.Next(colors.Count)];
+        }
+    }
+}
