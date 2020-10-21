6691ae49da7ce920d7bf6b15048b8e4941b5bdfa
ezsofi <e.zsofiakovacs@gmail.com>
2020-10-21 15:32:12 +0200

    HelloIdProject implemented

---
+++ week-07/day3/HelloDIProject/ConsoleLoggerMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloDIProject.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloDIProject
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ConsoleLoggerMiddleware : IMiddleware
+    {
+
+        private Printer MyPrinter;
+        public ConsoleLoggerMiddleware(Printer printer)
+        {
+            MyPrinter = printer;
+        }
+
+
+        private readonly RequestDelegate _next;
+        public ConsoleLoggerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        //public Task Invoke(HttpContext httpContext)
+        //{
+
+        //    return _next(httpContext);
+        //}
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Console.WriteLine("Hi from the middleware");
+            MyPrinter.Log("dasdfasfsd");
+
+            return next(context);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ConsoleLoggerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseConsoleLoggerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ConsoleLoggerMiddleware>();
+        }
+    }
+}
---
+++ week-07/day3/HelloDIProject/Startup.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using HelloDIProject;
+using HelloDIProject.Models;
+
+namespace HelloDIProject
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddTransient<ConsoleLoggerMiddleware>();
+            services.AddTransient<Printer>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseConsoleLoggerMiddleware();
+            app.UseMiddleware<ConsoleLoggerMiddleware>();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
